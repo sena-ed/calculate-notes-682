b2d0c731ad39119ecc5ab440e46d94c3e888e88d
kelvinocampo <kevinocampooso@gmail.com>
2024-10-08 13:27:02 -0500

    create variables HTML tag in js

---
+++ app.js
@@ -0,0 +1,12 @@
+// const $ = (el) => document.querySelector(el);
+
+const $name = document.getElementById('nombre')
+
+const $note1 = document.getElementById('note1')
+const $note2 = document.getElementById('note2')
+const $note3 = document.getElementById('note3')
+
+const $btnCalc = document.getElementById('btn-calculate')
+const $btnPredict = document.getElementById('btn-predict')
+
+const $calification = document.getElementById('resultado')
