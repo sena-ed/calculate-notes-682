79951a7d5e774470c5b90f4d94b46088494fc7ca
Jhonatan Arcos <116604745+davidar27@users.noreply.github.com>
2024-10-08 14:41:24 -0500

    correction

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const userName = document.getElementById('name')
-const data1 = document.getElementById('nota1')
-const data2 = document.getElementById('nota2')
-const data3 = document.getElementById('nota3')
+const data1 = document.getElementById('note1')
+const data2 = document.getElementById('note2')
+const data3 = document.getElementById('note3')
 const result = document.getElementById('result')
 
 
@@ -9,12 +9,9 @@ const btnCalculate = document.getElementById('btn-calculate')
 const btnPreview = document.getElementById('button')
 
 
-btnCalculate.addEventListener('click', calculeNoted) 
+btnCalculate.addEventListener('click', calculateNote) 
 
-
-
-function calculeNoted(event) {
-
+function calculateNote(event) {
     event.preventDefault()
 
     let name = userName.value
@@ -22,7 +19,7 @@ function calculeNoted(event) {
     let note2 = Number(data2.value)
     let note3 = Number(data3.value)
 
-    let result = (note1 * 0.3) + (note2 * 0.3) + (note3 * 0.4)
-    
-    response.textContent = `Señor@ ${name}, su nota deifinita es: ${result}`
+    let finalResult = (note1 * 0.3) + (note2 * 0.3) + (note3 * 0.4)
+
+    result.textContent = `Señor@ ${name}, su nota definitiva es: ${finalResult}`
 }
